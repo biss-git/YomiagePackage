5b7f85369a3b014f576c07410f50317103ea7f65
bisu_2525 <kotonohashimai@gmail.com>
2021-11-28 15:16:47 +0900

    v 1.2

---
+++ YomiageLibraryTemplate/VoiceLibrary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yomiage.SDK;
+
+namespace YomiageLibraryTemplate
+{
+    public class VoiceLibrary : VoiceLibraryBase
+    {
+        public override object GetValue(string command, string key)
+        {
+            return base.GetValue(command, key);
+        }
+    }
+}
---
+++ YomiageLibraryTemplateStandard/VoiceLibrary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Yomiage.SDK;
+
+namespace YomiageLibraryTemplateStandard
+{
+    public class VoiceLibrary : VoiceLibraryBase
+    {
+        public override object GetValue(string command, string key)
+        {
+            return base.GetValue(command, key);
+        }
+    }
+}
