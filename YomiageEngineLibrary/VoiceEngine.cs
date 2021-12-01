c5229d1a5281ce24d7d3f55e33ef4cdd4a32c7b0
bisu_2525 <kotonohashimai@gmail.com>
2021-12-02 02:05:05 +0900

    EngineLibrary を追加

---
+++ YomiageEngineLibrary/VoiceEngine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yomiage.SDK;
+using Yomiage.SDK.Talk;
+using Yomiage.SDK.VoiceEffects;
+
+namespace YomiageEngineLibrary
+{
+    public class VoiceEngine : VoiceEngineBase
+    {
+        public override async Task<double[]> Play(VoiceConfig mainVoice, VoiceConfig subVoice, TalkScript talkScript, MasterEffectValue masterEffect, Action<int> setSamplingRate_Hz)
+        {
+            return await base.Play(mainVoice, subVoice, talkScript, masterEffect, setSamplingRate_Hz);
+        }
+    }
+}
